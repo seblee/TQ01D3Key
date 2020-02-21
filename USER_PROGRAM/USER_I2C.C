0741c91f15ce47dbcd39767d65bba5fdb6a921ae
Seblee <ceeu@vip.qq.com>
2020-02-21 18:19:50 +0800

    Interrupt_MF2  as  pdf logic;

--- USER_PROGRAM/USER_I2C.C
+++ USER_PROGRAM/USER_I2C.C
@@ -7,28 +7,73 @@
 //==============================================
 void __attribute((interrupt(0x20))) Interrupt_MF2(void)
 {
+    uchar temp = 5;
+    _simf      = 0;
     if (_simtof)  // time out
     {
+        _simtoen = 1;
+        _simtof  = 0;
     }
-    else if (_simf)  // address match
+    else if (_haas)  // address match
     {
+        if (_srw)  // slave send
+        {
+            _htx  = 1;
+            _simd = 1;
+        }
+        else  // slave receive
+        {
+            _htx  = 0;
+            _txak = 0;
+            temp  = _simd;
+        }
     }
     else  // 8 bit trans ok
     {
-        /* code */
+        if (_htx)  // slave send
+        {
+            if (_rxak)
+            {
+                _htx  = 0;
+                _txak = 0;
+                temp  = _simd;
+            }
+            else
+            {
+                _simd = 0;
+            }
+        }
+        else  // slave receive
+        {
+            temp = _simd;
+        }
     }
 }
 void USER_I2C_INITIAL()
 {
-    _sim0       = 0;
-    _sim1       = 1;
-    _sim2       = 1;
-    __sima.byte = 0X02;
-    _sime       = 1;
-    _mf2e       = 1;
-    _simen      = 1;
+    _simc0 = 0b11000010;
+    //  _sim0 = 0;
+    // _sim1    = 1;
+    // _sim2    = 1;
+    // _simdeb0 = 0;
+    // _simdeb1 = 1;
+    // _simen   = 1;
+
+    _sima = 0xae;
+    _htx  = 0;
+    _txak = 0;
+    _simf = 0;
+
+    _simtoc = 0b10111111;
+
+    _sime = 1;
+    _mf2e = 1;
+    _emi  = 1;
 }
 
 void USER_I2C()
 {
+    uchar temp = 5;
+    temp++;
+    _sim0 = 0;
 }
